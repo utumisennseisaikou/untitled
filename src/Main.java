c38163c7a39064c6d0eaa48ed98151f008d4f4ba
unknown <241020>
2025-05-09 11:51:16 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main (String[] args){
+        System.out.println("陰浦蓮");
+        System.out.print("こんにちは。");
+        System.out.print("今日の天気は"+"\t晴れです。");
+        System.out.print("今日は一日\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は"+"\',文字列は\"で囲みましょう");
+    }
+
+
+}
