e7a3b06b0bcf354779ddb33647cb2ce006ddba22
kageurarenn <241020@std.hi-joho.ac.jp>
2025-05-16 12:26:59 +0900

    ファイル名間違えました

--- src/Review2.java
+++ src/Review2.java
@@ -1,4 +1,4 @@
-public class practice2 {
+public class Review2 {
     public static void main(String[] args){
         System.out.println(20+30+"50"+7+1);
         System.out.println(90+5-4+"5"+(5-10));
--- src/Review3.java
+++ src/Review3.java
@@ -1,4 +1,4 @@
-public class practice3 {
+public class Review3 {
     public static void main(String[] args){
         System.out.println(10-7*2+"7"+5*-10);
         System.out.println(3*2*3+7+"0"+5+3);
