844138eec8c247c33e53b0086262d9f2255dbc43
kageurarenn <241020@std.hi-joho.ac.jp>
2025-05-16 12:28:18 +0900

    課題１

---
+++ src/Review1.java
@@ -0,0 +1,12 @@
+public class Review1 {
+    public static void main (String[] args){
+
+        System.out.print("こんにちは。");
+        System.out.print("今日の天気は"+"\t晴れです。");
+        System.out.print("今日は一日\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は"+"\',文字列は\"で囲みましょう");
+    }
+
+
+}
