0f4d7efc2a893aa8c8a494da710d6c68485d4139
kageurarenn <241020@std.hi-joho.ac.jp>
2025-05-23 11:14:41 +0900

    pack

--- src/pack/kadai0.java
+++ src/pack/kadai0.java
@@ -1,4 +1,6 @@
-public class kutabare{
+package pack;
+
+public class kadai0 {
     public static void main(String[] args) {
         System.out.println("出力する文字を書き込む");
     }
--- src/pack/kutabare.java
+++ src/pack/kutabare.java
@@ -1,4 +1,6 @@
-public class kadai0 {
+package pack;
+
+public class kutabare{
     public static void main(String[] args) {
         System.out.println("出力する文字を書き込む");
     }
