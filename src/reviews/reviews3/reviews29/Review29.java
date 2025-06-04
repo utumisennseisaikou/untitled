cd2cf271e65d026ca7536bf6e17999db48a4650a
kageurarenn <241020@std.hi-joho.ac.jp>
2025-06-04 09:48:37 +0900

    課題

--- src/reviews/reviews3/reviews29/Review29.java
+++ src/reviews/reviews3/reviews29/Review29.java
@@ -1,8 +1,6 @@
 package reviews.reviews3.reviews29;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class Review29 {
     public static void main(String[] args) {
